7b171e8cd45a7d8fc3a913ce2b5f451277f8a581
John Gibbs <john.gibbs6526@mail.davistech.edu>
2025-03-19 15:05:22 -0600

    added tracklist option for one album

--- art.js
+++ art.js
@@ -1,15 +1,44 @@
-// fetching function that will be used later
+// fetching function that will be used when the user submits the form
+
+const key = "cIRvvnoIdclGUgBGdUOT";
+const secret = "whfMKUHqNtwljWbrEejkdGXmSbcYkAHe";
+const tokens = `&key=${key}&secret=${secret}`;
+
+// for searches
 
 const getDisc = async query => {
-    const resp = await fetch(`https://api.discogs.com/database/search?artist=${query}&per_page=3&key=cIRvvnoIdclGUgBGdUOT&secret=whfMKUHqNtwljWbrEejkdGXmSbcYkAHe`);
-    const data = await resp.json();
+    let page = Math.floor(Math.random() * 5) + 1;
 
-    if (resp.status != 200) {
-        throw new Error("Uh oh!");
+    // fetches
+
+    const search = await fetch(`https://api.discogs.com/database/search?artist=${query}&page=${page}&per_page=3${tokens}`);
+    const sData = await search.json();
+
+    const master = await fetch(`${sData.results[0].master_url}`);
+    const mData = await master.json();
+
+    if (search.status != 200 || master.status != 200) {
+        throw new Error("Search fetching failed.");
     }
 
-    return data;
+    console.log(sData);
+    console.log(mData);
+
+    return sData;
 };
+
+// for masters
+
+const getMaster = async masterURL => {
+    const master = await fetch(masterURL);
+    const mData = await master.json();
+
+    if (master.status != 200) {
+        throw new Error("Master fetching failed.");
+    }
+
+    return mData;
+}
 
 const form = document.querySelector("form");
 
@@ -19,17 +48,33 @@ form.addEventListener("submit", e => {
     let info = document.querySelector("#music-info");
     info.innerHTML = "";
 
-    getDisc(form.work.value).then(data => {
-        info.innerHTML += `<h1>${form.work.value}`;
+    let rand = Math.floor(Math.random() * 3) + 1;
 
-        for (let i = 0; i < 3; i++) {
+    // get the Discogs API data
+
+    getDisc(form.work.value)
+        .then(data => {
+
+            // input album
+
             info.innerHTML += `
+                <h1>${form.work.value}</h1>
                 <h2>Album</h2>
-                <div>${data.results[i].title}</div>
-                <hr>
+                <div>${data.results[rand].title}</div>
             `;
-        }
 
-        console.log(data);
+            return data.results[rand].master_url;
+        }
+    ).then(master => {
+        return getMaster(master);
+    }).then(data => {
+
+        // input tracklist
+        
+        info.innerHTML += "<h3>Tracklist</h3>";
+
+        data.tracklist.forEach(track => {
+            info.innerHTML += `<div>${track.title}</div>`;
+        });
     });
 });
